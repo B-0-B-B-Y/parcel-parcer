6a23a6b695aa5faa2c5e65a3743c61efd9bf98f2
Bob Naydenov <bob.naydenov@hotmail.com>
2020-02-14 22:51:12 +0000

    Filter by date, WIP: Separate into regions

--- index.js
+++ index.js
@@ -1,25 +1,39 @@
 const fs = require('fs')
+const csv = require('csvtojson')
 const args = process.argv.slice(2)
+const [ filepath, dateFilter ] = args
+let birminghamParcels, leedsParcels, wakefieldParcels
+let birminghamRegex = '(B)+'
 
 if (args.length != 2) {
     console.log("Please supply exactly 2 arguments to the script, like so ---> 'node index.js [filepathToCSV] [dateToFilterOn]'")
     process.exit(-1)
 }
 
-const filepath = args[0]
-const dateFilter = args[1]
+const filterByPostcode = (parcelData) => {
+    parcelData.forEach(parcel => {
+        birminghamParcels = parcel.postcode.match(birminghamRegex)
+    })
 
-const readInCSV = (filepath) => {
-    let parcelData
-
-    try {
-        parcelData = fs.readFileSync(filepath, 'utf8')
-    } catch (error) {
-        console.log('The following error occurred: \n\n', error)
-        process.exit(error.errno)
-    }
-
-    return parcelData
+    console.log(birminghamParcels)
 }
 
-readInCSV(filepath)
+const sortDepots = (filteredParcels) => {
+    // const birminghamParcels = filteredParcels.filter(parcel)
+    
+    filterByPostcode(filteredParcels)
+}
+
+const processCSV = (filepath) => {
+    csv()
+    .fromFile(filepath)
+    .then((parcelData) => {
+        const filteredParcels = parcelData.filter(parcel => {
+            return parcel.delivery_date === dateFilter.toString()
+        })
+
+        sortDepots(filteredParcels)
+    })
+}
+
+processCSV(filepath)
