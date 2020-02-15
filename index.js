07a60b4a64c8d670cb89860f9edda487c808f79c
Bob Naydenov <bob.naydenov@hotmail.com>
2020-02-15 15:55:37 +0000

    Working sorting algorithm for post codes

--- index.js
+++ index.js
@@ -2,26 +2,21 @@ const fs = require('fs')
 const csv = require('csvtojson')
 const args = process.argv.slice(2)
 const [ filepath, dateFilter ] = args
-let birminghamParcels, leedsParcels, wakefieldParcels
-let birminghamRegex = '(B)+'
+const birminghamCodes = [/B\d/, /TF/, /DY/, /WV/]
+const leedsCodes = [/WF16/, /BD/, /LS/]
+const wakefieldCodes = [/^WF1$/, /WF2/, /WF3/, /WF4/, /HD/]
 
 if (args.length != 2) {
     console.log("Please supply exactly 2 arguments to the script, like so ---> 'node index.js [filepathToCSV] [dateToFilterOn]'")
     process.exit(-1)
 }
 
-const filterByPostcode = (parcelData) => {
-    parcelData.forEach(parcel => {
-        birminghamParcels = parcel.postcode.match(birminghamRegex)
-    })
-
-    console.log(birminghamParcels)
-}
-
 const sortDepots = (filteredParcels) => {
-    // const birminghamParcels = filteredParcels.filter(parcel)
-    
-    filterByPostcode(filteredParcels)
+    for (x = 0; x < filteredParcels.length; x++) {        
+        if (wakefieldCodes.some(code => filteredParcels[x].postcode.split(' ')[0].match(code))) {
+            console.log(filteredParcels[x].postcode)
+        }
+    }
 }
 
 const processCSV = (filepath) => {
