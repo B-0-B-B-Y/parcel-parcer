4f91bc472643bc247813b3b6dbc55959639078b8
Bob Naydenov <bob.naydenov@hotmail.com>
2020-02-15 17:08:52 +0000

    Add route information and eta from API results

--- index.js
+++ index.js
@@ -1,22 +1,41 @@
 const fs = require('fs')
 const csv = require('csvtojson')
+const axios = require('axios')
 const args = process.argv.slice(2)
 const [ filepath, dateFilter ] = args
 const birminghamCodes = [/B\d/, /TF/, /DY/, /WV/]
 const leedsCodes = [/WF16/, /BD/, /LS/]
-const wakefieldCodes = [/^WF1$/, /WF2/, /WF3/, /WF4/, /HD/]
 
 if (args.length != 2) {
     console.log("Please supply exactly 2 arguments to the script, like so ---> 'node index.js [filepathToCSV] [dateToFilterOn]'")
     process.exit(-1)
 }
 
-const sortDepots = (filteredParcels) => {
-    for (x = 0; x < filteredParcels.length; x++) {        
-        if (wakefieldCodes.some(code => filteredParcels[x].postcode.split(' ')[0].match(code))) {
-            console.log(filteredParcels[x].postcode)
+const sortParcels = async (filteredParcels) => {
+    const birminghamParcels = [], leedsParcels = [], wakefieldParcels = []
+
+    for (x = 0; x < filteredParcels.length; x++) {
+        const currentParcel = filteredParcels[x]
+        const postcodeID = currentParcel.postcode.split(' ')[0]
+        const deliveryInformation = await axios.get(`https://us-central1-dpduk-s-test-d1.cloudfunctions.net/parcels/${currentParcel.parcel_number}`,
+         { headers: {'Authorization' : `Bearer 04B5BD62-A454-41D0-BEEB-E952D239283E`}
+        }).then((result) => {
+            return result.data
+        })
+
+        currentParcel.route = deliveryInformation.route
+        currentParcel.eta = deliveryInformation.eta
+
+        if (birminghamCodes.some(code => postcodeID.match(code))) {
+            birminghamParcels.push(currentParcel)
+        } else if (leedsCodes.some(code => postcodeID.match(code))) {
+            leedsParcels.push(currentParcel)
+        } else {
+            wakefieldParcels.push(currentParcel)
         }
     }
+
+    return { birminghamParcels, leedsParcels, wakefieldParcels }
 }
 
 const processCSV = (filepath) => {
@@ -27,7 +46,10 @@ const processCSV = (filepath) => {
             return parcel.delivery_date === dateFilter.toString()
         })
 
-        sortDepots(filteredParcels)
+        return sortParcels(filteredParcels)
+    })
+    .then((sortedParcels) => {
+        console.log(sortedParcels)
     })
 }
 
